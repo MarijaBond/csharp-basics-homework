3cf3748e03fec233b4c6674350cadf625ad52921
MarijaBond <marija.bondarenko@inbox.lv>
2019-08-23 13:40:47 +0300

    added TypesAndVariables

---
+++ TypesAndVariables/Exercise  7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise__7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that prompts the user to enter a string and displays the number of the uppercase letters in the string.
+            Console.Write("Enter a string!");
+            string userInput = Console.ReadLine();
+
+            int countUppercaseChars = 0;
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (char.IsUpper(userInput[i]))
+                    countUppercaseChars++;
+            }
+
+
+            Console.WriteLine($"The number of the uppercase letters in your string is {countUppercaseChars}.");
+        }
+    }
+}
