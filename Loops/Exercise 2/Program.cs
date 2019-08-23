06150574d3af861ddf57a581014c380a349cd41b
MarijaBond <marija.bondarenko@inbox.lv>
2019-08-23 22:54:23 +0300

    added Loops

---
+++ Loops/Exercise 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+
+            Console.WriteLine("Input number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input value of i : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            int result = i;
+
+            for (var a = 1; a <= n - 1; a++)
+            {
+
+                result = result * i;
+
+            }
+
+            Console.WriteLine("The result is: " + result);
+
+            /*
+            todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.Pow()
+            for (.......) {
+              Console.WriteLine(........);
+            }
+            */
+        }
+    }
+}
---
+++ Loops/Exercise 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        //TODO: print all vowels using for and foreach
+        static void Main(string[] args)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+
+            for (var i = 0; i < vowels.Length; i++)
+            {
+                Console.WriteLine(vowels[i]);
+            }
+
+
+            foreach (char items in vowels)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
