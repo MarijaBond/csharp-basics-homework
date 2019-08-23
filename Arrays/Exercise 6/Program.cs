0d3a59844374d7fa3e6d2a910fda9f859d296880
MarijaBond <marija.bondarenko@inbox.lv>
2019-08-23 12:46:56 +0300

    added Arrays

---
+++ Arrays/Exercise 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            Random x = new Random();
+
+            int[] myBestArrayYet = new int[10];
+            int[] myBestArrayYetCopy = new int[10];
+
+            for (i = 0; i < 10; i++)
+            {
+                int myRandomNumber = x.Next(1, 100);
+                myBestArrayYet[i] = myRandomNumber;
+                myBestArrayYetCopy[i] = myBestArrayYet[i];
+            }
+
+            myBestArrayYet[9] = -7;
+
+            Console.WriteLine(string.Join(",", myBestArrayYet));
+            Console.WriteLine(string.Join(",", myBestArrayYetCopy));
+
+        }
+    }
+}
